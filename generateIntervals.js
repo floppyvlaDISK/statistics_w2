8c50ee97c22d85f0b5833e3e667048e555a9bcd0
vlad <tarasenkokansasvlad@gmail.com>
2020-10-31 09:44:00 +0200

    Init first step

---
+++ generateIntervals.js
@@ -0,0 +1,15 @@
+function generateIntervals(coordinatePairs) {
+  console.log(typeof coordinatePairs, coordinatePairs);
+  const stepX = 0;
+  const stepY = 0;
+  const intervalsX = [];
+  const intervalsY = [];
+  return {
+    stepX,
+    stepY,
+    intervalsX,
+    intervalsY,
+  };
+}
+
+module.exports = generateIntervals;
---
+++ index.js
@@ -0,0 +1,4 @@
+const generateIntervals = require("./generateIntervals");
+const data = require("./data.json");
+
+console.log(generateIntervals(data));
